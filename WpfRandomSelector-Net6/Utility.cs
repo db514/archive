51bfe7be6fac3e721b38727f0d59afc5a5179522
db514 <db514@hotmail.com>
2023-12-29 11:15:45 -0500

    Add files via upload

---
+++ WpfRandomSelector-Net6/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfRandomSelector
+{
+    public class Utility
+    {
+        public static bool IsPathValid(string path)
+        {
+            try
+            {
+                _ = Path.GetFullPath(path);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
